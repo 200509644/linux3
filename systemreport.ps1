d114e16f920f9af736aa2b22542c1396c4daef9e
200509644 <138265547+200509644@users.noreply.github.com>
2023-08-14 19:56:17 -0400

    Lab 5

---
+++ systemreport.ps1
@@ -0,0 +1,27 @@
+﻿param ( [switch]$system,
+	    [switch]$disk,
+	    [switch]$network
+)
+import-module Module200509644.psm1
+if ($system -eq $false -and $disk -eq $false -and $network -eq $false) {
+cpu11
+os11
+ram11
+video11
+disk11
+network11
+}
+else {
+if ($system) {
+cpu11
+os11
+ram11
+video11
+}
+if ($disk) {
+disk11
+}
+if ($network) {
+network11
+}
+}
